0b71a8243d1a50081ddfa0661d2144186198296d
Lautaro <lautao.rodriguez01@globant.com>
2021-07-21 18:05:54 -0300

    move reducer outside component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,29 +7,11 @@ import Delivery from './Delivery.jsx';
 import background from '../../public/images/TexturesCom_WoodPlanksOld0292_7_seamless_S.jpg'
 import './App.css';
 
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { addReducer } from './reducers/addProdReducer'
 
-const initialState = {
-    products: [],
-    listProds : [
-        { title: 'Cervezas', products: [{ name:'Doble Ipa', desc:'Que rica la doble ipa rica la doble ipa rica la doble ipa rica la doble ipa rica la doble ipa', price:'$1000' }, { name:'Blone Ale', desc:'Es como la cerveza rubia normal', price:'$100' }] },
-        { title: 'Comida', products: [{ name:'Papas con cheddar', desc:'Papas delciosas con cheddar', price:'150' }, { name:'Pizza con muzzarela', desc:'Una pizzeta bien cheta y rica', price:'$200' }] }
-    ]
-};
-
-function reducer(state = initialState, action) {
-    switch (action.type){
-        case 'ADD':
-            let newProducts = state.products;
-            newProducts.push(action.prod);
-            return { products: newProducts, listProds: state.listProds };
-        default:
-            return state;
-    }
-}
-
-const store = createStore(reducer);
+const store = createStore(addReducer);
 
 const App = () => {
     return (
